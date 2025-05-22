d9377f52c554b0585eaeb84af5a3693a45485080
Adeniyi Bello <poph2@users.noreply.github.com>
2025-05-22 19:48:00 -0400

    fix: Update @hiveops/node and @hiveops/cli versions in package.json and package-lock.json; refactor user creation logic in app.ts

--- vespa-typescript/src/app.ts
+++ vespa-typescript/src/app.ts
@@ -8,44 +8,44 @@ configDotenv();
 const start = async () => {
   vespaInit();
 
-  // const firstName = faker.person.firstName();
-  // const lastName = faker.person.lastName();
-  // const email = faker.internet.email({ firstName, lastName });
+  const firstName = faker.person.firstName();
+  const lastName = faker.person.lastName();
+  const email = faker.internet.email({ firstName, lastName });
 
-  // const user = await userRepository.saveOne({
-  //   firstName,
-  //   lastName,
-  //   email,
-  //   age: faker.number.int({ min: 21, max: 45 }),
-  //   isActive: faker.datatype.boolean(),
-  //   lastLogin: new Date(Date.now()),
-  //   profileImage: {
-  //     url: faker.image.avatar(),
-  //     width: faker.number.int({ min: 100, max: 500 }),
-  //     height: faker.number.int({ min: 100, max: 500 }),
-  //     alt: faker.lorem.words(3),
-  //   },
-  //   role: UserRole.ADMIN,
-  // });
-  // console.log(user);
+  const user = await userRepository.saveOne({
+    firstName,
+    lastName,
+    email,
+    age: faker.number.int({ min: 21, max: 45 }),
+    isActive: faker.datatype.boolean(),
+    lastLogin: new Date(Date.now()),
+    profileImage: {
+      url: faker.image.avatar(),
+      width: faker.number.int({ min: 100, max: 500 }),
+      height: faker.number.int({ min: 100, max: 500 }),
+      alt: faker.lorem.words(3),
+    },
+    role: UserRole.ADMIN,
+  });
+  console.log(user);
 
-  // if (!user) {
-  //   console.error("User not found");
-  //   return;
-  // }
+  if (!user) {
+    console.error("User not found");
+    return;
+  }
 
-  // const post = await postRepository.saveOne({
-  //   userId: user.id,
-  //   title: faker.lorem.words(3),
-  //   body: faker.lorem.paragraph(),
-  // });
-  // console.log(post);
+  const post = await postRepository.saveOne({
+    userId: user.id,
+    title: faker.lorem.words(3),
+    body: faker.lorem.paragraph(),
+  });
+  console.log(post);
 
   const users = await userRepository.findMany({});
   console.log(users);
 
-  // const posts = await postRepository.findMany({});
-  // console.log(posts);
+  const posts = await postRepository.findMany({});
+  console.log(posts);
 
 
 };
